cd76840daf021445b3a4c5e7b3b23e62ac38e28f
SuBHo <sayandhar676@gmail.com>
2026-07-28 16:58:23 +0000

    learning comparisons in js

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,3 +1,16 @@
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+console.log(null > 0) //false
+console.log(null == 0) //false 
+console.log(null >= 0) //true
+
+console.log(undefined >= 0) //false 
+console.log(undefined == 0) //false 
+console.log(undefined === 0) //false 
+console.log(undefined > 0) //false 
+
+// avoid these kind of comparisons at any cost cuz the output is not reliable
+
+console.log("2" == 2) //true
+console.log("2" === 2) //false
+
+// === => checks datatype also unlike == which checks value only
+
